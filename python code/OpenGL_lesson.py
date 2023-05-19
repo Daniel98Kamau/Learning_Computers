19d887ff096f5d77b6abc95b362db862121e3ce7
Daniel98Kamau <128593418+Daniel98Kamau@users.noreply.github.com>
2023-05-19 11:10:55 +0300

    Modified

--- python code/OpenGL_lesson.py
+++ python code/OpenGL_lesson.py
@@ -4,11 +4,11 @@ from OpenGL.GLU import * # without use of the dot notation.
 from OpenGL.GLUT import *
 import sys   #provides some housekeeping tools to create the graphics display. 
 
-#Draw a wireteapot of the specified size
+#Draw a teapot of the specified size
 def draw():
     glClear(GL_COLOR_BUFFER_BIT)
-    #glutWireTeapot(1)
-    glutSolidTeapot(0.5)
+    #glutWireTeapot(1) #Wire teapot
+    glutSolidTeapot(0.5) #Solid teapot
     glFlush()
 
 glutInit(sys.argv) #Displaying GLUT style graphics.
@@ -17,5 +17,35 @@ glutInitWindowSize(800, 600)
 glutInitWindowPosition(100,100)
 glutCreateWindow("Teapot") #Sets title of display window
 glutDisplayFunc(draw) #tells GLUT where to find the function that creates the graphics scene.
-glutMainLoop() #Starts the program
+#glutMainLoop() #Starts the program
 
+
+def init(): #Initialises the plotting process
+    glClearColor(1.0, 0.0, 1.0 , 1.0) #sets background color(R,G,B,A)
+    gluOrtho2D(-8.0, 8.0, -8.0, 8.0) #sets origin at the centre of the window
+
+#Drawing a point at the centre of window
+def plot_point():
+    glClear(GL_COLOR_BUFFER_BIT) #Clears the sreen, sets background colour
+    glColor3f(1.0, 1.0, 1.0) #sets color of plotting pen
+    glPointSize(10.0) #sets the size of the point
+    glBegin(GL_POINTS)
+    glVertex2f(0, 0) #Sets position of point to be drawn
+    glVertex2f(4,0)
+    glVertex2f(-3,-3)
+    glVertex2f(2,-4)
+
+    glEnd()
+    glFlush()
+
+def main():
+    glutInit(sys.argv)
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
+    glutInitWindowSize(500, 500)
+    glutInitWindowPosition(50, 50)
+    glutCreateWindow("Central Point")
+    glutDisplayFunc(plot_point)
+    init()
+    glutMainLoop()
+
+main()
--- python code/pypy.py
+++ python code/pypy.py
@@ -1,38 +1,35 @@
-import pygame
-from pygame.locals import*
+from OpenGL.GLUT import *
 from OpenGL.GL import*
 from OpenGL.GLU import*
+import sys
 
 Vertices = ((0,0,0), (4,0,0), (4,4,0), (0,4,0),
             (0,4,4), (0,0,4),(4,0,4), (4,4,4))
 Edges = ((0,1), (0,3), (0,5), (2,1), (2,3), (2,7),
          (4,3), (4,5), (4,7), (6,1), (6,5), (6,7))
 
+def init():
+    glClearColor(0.0, 0.0, 0.0, 1.0)
+    gluOrtho2D(-1.0, 5.0, -1.0, 5.0)
+
 def Cube():
+    glClear(GL_COLOR_BUFFER_BIT)
+    glColor3f(1.0, 0.0, 1.0)
     glBegin(GL_LINES)
     for edge in Edges:
         for vertex in edge:
             glVertex3fv(Vertices[vertex])
     glEnd()
+    glFlush()
 
 def main():
-    pygame.init()
-    display = (800,600)
-    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
-
-    gluPerspective(45, 800/600, 0.1, 50.0)
-    glTranslatef(0.0, 0.0, -5.0)
-    glRotatef(0, 0, 0, 0)
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-        Cube()
-        pygame.display.flip()
-        pygame.time.wait(5)
+    glutInit(sys.argv)
+    glutInitWindowSize(800,800)
+    glutInitWindowPosition(50,50)
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
+    glutCreateWindow("Cube")
+    glutDisplayFunc(Cube)
+    init()
+    glutMainLoop()
 
 main()
